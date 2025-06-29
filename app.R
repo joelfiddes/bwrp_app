b68f2144ccad299ce4becbf168d6b497a056a406
Joel Fiddes <joelfiddes@gmail.com>
2025-06-29 21:12:58 +0200

    add annual ET

--- app.R
+++ app.R
@@ -19,13 +19,38 @@ catchments$PET_mm_annual_penman   <- catchments$PET_mm_hr_penman   * 8760
 variables <- c('LWin', 'SWin', 'Tair_C', 'RH', 'wind_ms', 
                'TP_mmhr', 'Sf_mmhr', 'Rf_mmhr', 'Tsurf', 'LWout', 'LWnet', 'SWnet', 
                'Rnet_Wm2', 'Rnet_MJm2hr', 'PET_mm_hr_penman', 'delta', 'PET_mm_hr_priestly', 'Annual_TP_mm', 'PET_mm_annual_priestly', 'PET_mm_annual_penman')
+               
+               variable_labels <- c(
+  'LWin' = "Incoming Longwave Radiation [W/m²]",
+  'SWin' = "Incoming Shortwave Radiation [W/m²]",
+  'Tair_C' = "Air Temperature [°C]",
+  'RH' = "Relative Humidity [%]",
+  'wind_ms' = "Wind Speed [m/s]",
+  'TP_mmhr' = "Total Precipitation [mm/hr]",
+  'Sf_mmhr' = "Snowfall [mm/hr]",
+  'Rf_mmhr' = "Rainfall [mm/hr]",
+  'Tsurf' = "Surface Temperature [°C]",
+  'LWout' = "Outgoing Longwave Radiation [W/m²]",
+  'LWnet' = "Net Longwave Radiation [W/m²]",
+  'SWnet' = "Net Shortwave Radiation [W/m²]",
+  'Rnet_Wm2' = "Net Radiation [W/m²]",
+  'Rnet_MJm2hr' = "Net Radiation [MJ/m²/hr]",
+  'PET_mm_hr_penman' = "Penman PET [mm/hr]",
+  'delta' = "Clausius-Clapeyron Slope [kPa/°C]",
+  'PET_mm_hr_priestly' = "Priestly-Taylor PET [mm/hr]",
+  'Annual_TP_mm' = "Annual Precipitation [mm/year]",
+  'PET_mm_annual_priestly' = "Annual PET (Priestly-Taylor) [mm/year]",
+  'PET_mm_annual_penman' = "Annual PET (Penman) [mm/year]"
+)
+
 
 # UI
 ui <- fluidPage(
   titlePanel("Baluchistan Climate Explorer"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("selected_var", "Select Forcing Variable:", choices = variables)
+      selectInput("selected_var", "Select Forcing Variable:", choices = variable_labels)
+
     ),
     mainPanel(
       leafletOutput("map", height = "800px")
